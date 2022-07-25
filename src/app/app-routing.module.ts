0d19eb55c57095df24d951d216dc2bf533d819f0
CristianKmilo007 <cc.rojas.07@gmail.com>
2022-07-25 18:19:18 -0500

    Se agrega el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegistroComponent } from './views/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component:HomeComponent},
+  {path: "login", component:LoginComponent},
+  {path: "registroUsers", component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegistroComponent } from './views/registro/registro.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { SectionsComponent } from './components/sections/sections.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    CarruselComponent,
+    SectionsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
